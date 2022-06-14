f6d41c893c4c2f805bf8c293030c0e23bc11fe98
Luuk Schouten <Luukschouten01@gmail.com>
2022-06-14 16:35:31 +0200

    intial commit/added create task functionality

---
+++ create.php
@@ -0,0 +1,17 @@
+<?php 
+
+include 'connection.php';
+
+if(isset($_POST['submit'])){
+
+    $name  = $_POST['name'];
+    $description = $_POST['description'];
+    $deadline = $_POST['deadline'];
+
+    $query = "INSERT INTO tasks (name, description, deadline) VALUES ('$name', '$description', '$deadline')";
+    $conn->exec($query);
+
+    //prevent multiple injections on refresh by going back to index
+    header("Location: index.php");
+
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type=text/css href="css/index.css">
+    <title>ToDo list</title>
+</head>
+<body>
+
+<div class='main'>
+
+    <form method='post' action='create.php'>
+        <input type='text' placeholder='Naam' name='name'><br>
+        <input type='text' placeholder='Taakomschrijving' name='description'><br>
+        <input type='date' name='deadline'><br>
+        <input type='submit' name='submit' value='Toevoegen'><br>
+    </form><br>
+
+    <?php include 'read.php'; ?>
+
+</div>
+    
+</body>
+</html>
